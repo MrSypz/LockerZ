0c4cb09c70715a08c39993910c86a3f9f666de4d
chai <chainot@hotmail.co.th>
2024-12-06 22:20:36 +0700

    drop down menu

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,13 +1,8 @@
 import './globals.css'
-import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { ThemeProvider } from "@/components/theme-provider"
 
 const inter = Inter({ subsets: ['latin'] })
-
-export const metadata: Metadata = {
-    title: 'LockerZ',
-    description: 'Secure file management system',
-}
 
 export default function RootLayout({
                                        children,
@@ -15,15 +10,17 @@ export default function RootLayout({
     children: React.ReactNode
 }) {
     return (
-        <html lang="en">
+        <html lang="en" suppressHydrationWarning>
         <body className={inter.className}>
-        <div className="flex h-screen">
-            <div className="flex flex-col flex-1 overflow-hidden">
-                <main className="flex-1 overflow-auto">
-                    {children}
-                </main>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <div className="flex h-screen bg-background text-foreground">
+                <div className="flex flex-col flex-1 overflow-hidden">
+                    <main className="flex-1 overflow-auto">
+                        {children}
+                    </main>
+                </div>
             </div>
-        </div>
+        </ThemeProvider>
         </body>
         </html>
     )
