f0263445ca994f8f6f8d097b52473d97205e4c97
chai <chainot@hotmail.co.th>
2024-12-11 02:50:40 +0700

    bumb

--- backend/job.js
+++ backend/job.js
@@ -6,7 +6,7 @@ const ext = process.platform === 'win32' ? '.exe' : '';
 const rustInfo = execSync('rustc -vV');
 const targetTriple = /host: (\S+)/g.exec(rustInfo)[1];
 if (!targetTriple) {
-    console.error('Failed to determine platform target triple');
+    logger.info('Failed to determine platform target triple');
 }
 fs.renameSync(
     `dist/app${ext}`,
