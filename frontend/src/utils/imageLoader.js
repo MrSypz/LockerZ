7466f974660a10ba473f1a30049b2f8a756b6b07
chai <chainot@hotmail.co.th>
2024-12-13 22:18:07 +0700

    unused

--- frontend/src/utils/imageLoader.js
+++
@@ -1,9 +0,0 @@
-export default function imageLoader({ src, width, quality }) {
-    const url = new URL(src, process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001');
-    url.searchParams.set('w', width);
-    if (quality) {
-        url.searchParams.set('q', quality);
-    }
-    return url.toString();
-}
-
