297cca2a194446fff8ef9c41ab51440f03668b01
chai <chainot@hotmail.co.th>
2024-12-15 15:46:36 +0700

    bumb

--- lockerz/next.config.ts
+++ lockerz/next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === 'production';
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const internalHost = 'localhost';
 
+const nextConfig: import('next').NextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: 'localhost',
+                port: '3001',
+            },
+        ],
+        unoptimized: true,
+    },
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        // Nah it work is work!!!!!!!!!!!!!!!!!!!!!!!!
+        ignoreBuildErrors: true,
+    },
+    output: "export",
+    assetPrefix: isProd ? undefined : `http://${internalHost}:3000`,
+}
 export default nextConfig;
