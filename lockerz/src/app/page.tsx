009e52604365e797dc3e2483420fe1075cabd9ed
chai <chainot@hotmail.co.th>
2024-12-19 07:38:13 +0700

    incorrect order

--- lockerz/src/app/page.tsx
+++ lockerz/src/app/page.tsx
@@ -4,6 +4,19 @@ import {Changelog} from "@/components/widget/Changelog"
 const changelogItems = [
     {
         version: "0.2.5 - beta",
+        date: "16-12-2024",
+        sections: [
+            {
+                title: "Fix",
+                type: "feature",
+                content: `
+              <h3 class="text-lg font-semibold mb-2">Node.js</h3> Now remove archive it size smaller around 4kb
+              <h3 class="text-lg font-semibold mb-2">Backend</h3> Fix Image Process Before linear to Area`
+            },
+        ]
+    },
+    {
+        version: "0.2.4 - beta",
         date: "18-12-2024",
         sections: [
             {
@@ -27,19 +40,6 @@ const changelogItems = [
               <h1 class="text-lg font-semibold mb-2">ContextMenu - Update</h1>
               <p class="mb-4">Now it can open the image folder directory!</p>
               `
-            },
-        ]
-    },
-    {
-        version: "0.2.4 - beta",
-        date: "16-12-2024",
-        sections: [
-            {
-                title: "Fix",
-                type: "feature",
-                content: `
-              <h3 class="text-lg font-semibold mb-2">Node.js</h3> Now remove archive it size smaller around 4kb
-              <h3 class="text-lg font-semibold mb-2">Backend</h3> Fix Image Process Before linear to Area`
             },
         ]
     },
