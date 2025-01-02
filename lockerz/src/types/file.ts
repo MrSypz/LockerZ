37da11e636b1a83b126894b60c0139aab747059e
chai <chainot@hotmail.co.th>
2025-01-02 08:40:22 +0700

    interface for tauri invoke

--- lockerz/src/types/file.ts
+++ lockerz/src/types/file.ts
@@ -1,10 +1,16 @@
 export interface File {
     name: string;
     category: string;
-    url: string;
     filepath: string;
     size: number;
     tags?: string[];
     lastModified: string;
+}
+
+export interface FileResponse {
+    files: File[];
+    current_page: number;
+    total_pages: number;
+    total_files: number;
 }
 
