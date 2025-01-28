09f981ac46fc0ac6b1a281af3b3d56cf879a4888
chai <chainot@hotmail.co.th>
2025-01-28 21:01:22 +0700

    delete changelog no need btw

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import {Dashboard} from "@/components/widget/Dashboard"
-import {Changelog} from "@/components/widget/Changelog"
 
 export default function Home() {
 
@@ -8,7 +7,6 @@ export default function Home() {
             <main className="flex-1 overflow-y-auto p-4 sm:p-6 lg:p-8">
                 <div className="max-w-7xl mx-auto space-y-8">
                     <Dashboard/>
-                    <Changelog/>
                 </div>
             </main>
         </div>
