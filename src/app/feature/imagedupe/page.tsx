9ef78c637376129e6335a72eb56277ff1a7a4252
chai <chainot@hotmail.co.th>
2025-01-30 02:24:06 +0700

    translate

---
+++ src/app/feature/imagedupe/page.tsx
@@ -0,0 +1,15 @@
+import DuplicateChecker from "@/components/widget/DuplicateChecker"
+import React from "react";
+
+export default function ImageDupe() {
+    return (
+        <div className="flex flex-col h-screen">
+            <main className="flex-1 overflow-y-auto p-4 sm:p-6 lg:p-8">
+                <div className="max-w-7xl mx-auto space-y-8">
+                    <h1 className="text-3xl font-bold mb-6 text-foreground">Image Dupe Checker</h1>
+                    <DuplicateChecker/>
+                </div>
+            </main>
+        </div>
+    )
+}
