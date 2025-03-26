3a95b91187b93ef1d0f638e65b850c95c78a241c
chain <chainot@hotmail.co.th>
2025-03-27 00:51:00 +0700

    move

--- src/components/dialog/tags/TagList.tsx
+++ src/components/dialog/tags/TagList.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { AlertCircle } from "lucide-react"
-import {TagItem} from "@/components/dialog/TagItem";
+import {TagItem} from "@/components/dialog/tags/TagItem";
 import {TagInfo} from "@/hooks/use-database";
 
 interface TagListProps {
